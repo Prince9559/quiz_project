e37f1a27f0eed406cc0b44f7518810d6197a6d3e
Prince Software <kumarprince95596186@gmail.com>
2025-07-01 01:29:21 +0530

    quiz

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,7 +8,7 @@ function Home() {
 
     <div className="home">
     
-      <h1 className='fading'>👉🏼 Welcome to My Quiz App</h1>
+      <h1 className='pri'>👉🏼 Welcome to My Quiz App</h1>
 
       <div className="button-container">
         <Link to="/quiz">
